688a0dcecba45630cbff4cfc8ccb3873b4aef4cf
mthieden <m.thieden@gmail.com>
2016-09-08 16:00:56 +0200

    uge24 updated

--- uge2/uge24.c
+++ uge2/uge24.c
@@ -1,6 +1,6 @@
-  
   float total = 0;
-
+  int forsendelse = 0;
+  float rabat;
   int antalTotal = 0;
   for (int i = 0; i < counter; i++) {
     printf("\t\t=======\n");
@@ -9,17 +9,22 @@
     printf("\t\tPris/stk:%d\n",pris[i]);
     total += antal[i]*pris[i];
     antalTotal += antal[i];
-    printf("\t\tantaltotal: %d\n",antalTotal);
   } //end of for loop (print varer)
   
+
+  printf("\n\t\tsamlet pris:%f\n",total);
   if(total > 500)
   {
-    total = total * 0.9;
+    rabat = total * 0.1;
+    total *= 0.9;
   }
-  else if (total < 200 )
+  else if (total < 200 || antalTotal < 3)
   {
     total += 50;
+    forsendelse += 50;
   }
-
-  printf("\n\t\ttotal:%f\n",total);
+  
+  printf("\n\t\tforsendelse:%d\n",forsendelse);
+  printf("\n\t\trabat:%f\n",rabat);
+  printf("\n\t\ttotal pris:%f\n",total);
 
