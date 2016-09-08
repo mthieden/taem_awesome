6f0473622ee26b002bd5aaabbf3472d7a134b3af
mthieden <m.thieden@gmail.com>
2016-09-08 10:54:42 +0200

    tilføjet kodefil, med initialisering

---
+++ uge2/src/uge2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int counter = 0;
+int maksBest = 10;
+int flereVare = 1;
+int vareNr[maksBest];
+int pris[maksBest];
+int antal[maksBest];
+
+
+
+
+
+int main()
+{
+return 0;
+
+}
