44c19d199a6eb8af2069cc90a555da6c6b3c94ed
Jonas Ladefoged Holm <jonas.dk.1990@gmail.com>
2016-09-15 12:11:15 +0200

    Lommeregner Done - comments
    Mangler comments

--- uge3/uge_32.c
+++ uge3/uge_32.c
@@ -7,17 +7,45 @@ int main (void){
 
   double tal1;
   double tal2;
+  double res;
 
   printf("Indtast det første tal: ");
-  scanf("%f\n", &tal1);
+  scanf(" %lf", &tal1);
 
   printf("Indtast det andet tal: ");
-  scanf("%f\n", &tal2);
+  scanf(" %lf", &tal2);
 
-  printf("%f\n",tal1 );
-  printf("%f\n",tal2 );
+  printf("%.2lf\n", tal1);
+  printf("%.2lf\n", tal2);
 
-  gets();
+  printf("Should they be:\n1: Added together? \n2: Or multiplied?\n");
+
+  //for at træne brugen af enums gør jeg brug af dem her
+  enum brugerValg {intet, multiplied, added};
+  enum brugerValg currentValg = intet;
+
+  while (currentValg != multiplied && currentValg != added){
+    int valg;
+    scanf(" %d", &valg);
+    if (valg == 1){
+      currentValg = added;
+    } else if (valg == 2) {
+      currentValg = multiplied;
+    } else {
+      printf("Wrong answer try again!\n");
+    }
+  }
+
+  switch (currentValg) {
+    case added:
+      res = tal1 + tal2;
+      break;
+    case multiplied:
+      res = tal1 * tal2;
+      break;
+  }
+
+  printf("%.2lf\n",res);
 
   return(0);
 
