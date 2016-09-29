7be4c9fe6983ff5aa56b1ce1d128c665f0434845
Jonas Holm <jonas.dk.1990@gmail.com>
2016-09-29 12:53:15 +0200

    sidste opgave færdig

--- uge5/uge54.c
+++ uge5/uge54.c
@@ -2,7 +2,7 @@
 #include<time.h>
 
 time_t rawtime;
-struct tm * timeinfo;
+struct tm *timeinfo;
 int alarm[] = {0,0,0};
 
 void show_time(void);
@@ -15,29 +15,61 @@ void snooze(void);
 
 void stop_alarm(void);
 
+void power_off(void);
+
+int menuRunner = 1;
+int choice;
+
 int main()
 {
-    
-    while(1)
+
+void (*menu[6])(void) = { show_time,
+                          set_time,
+                          set_alarm,
+                          snooze,
+                          stop_alarm,
+                          power_off};
+
+  while (menuRunner != 0)
+  {
+
+    printf("==== Menu ====\n");
+    printf("TidsModus:\t1\n");
+    printf("Indstil tid:\t2\n");
+    printf("Indstil alarm:\t3\n");
+    printf("Snooze alarm:\t4\n");
+    printf("Stop alarm:\t5\n");
+    printf("Hiv stik ud:\t6\n");
+
+    int choiceChecker = 1;
+
+    while (choiceChecker)
     {
-        show_time();
-        set_time();
-        set_alarm();
-        show_time();
-        snooze();
-        show_time();
-        stop_alarm();
-        show_time();
-    }
-    return 0;
-}
+      scanf(" %d",&choice);
+
+      if (0 < choice && choice < 7)
+      {
+        choiceChecker = 0;
+      } else {
+        printf("Prov igen\n");
+      }//end if else
+
+     } //end choiceChecker loop
+
+    choice--;
+    (*menu[choice])();
+
+  }//end of menuRunner
+  sluk:
+  return 0;
+}//end of main
 
 void show_time()
 {
     time ( &rawtime);
     timeinfo = localtime ( &rawtime );
-    
-    if(alarm[0] == 1 && alarm[1] == timeinfo->tm_hour, alarm[2] == timeinfo->tm_min )
+
+    if(alarm[0] == 1 && alarm[1] == timeinfo->tm_hour && alarm[2] == timeinfo->tm_min )
     {
         printf("ALARM!!!!!!!!!!!!!\n");
     }
@@ -63,10 +95,19 @@ void set_alarm(void)
 void snooze(void)
 {
     alarm[2] = (alarm[2]+5)%60;
+    printf("Alarmen er blevet udskudt! med 5 min\n");
 }
 
 void stop_alarm(void)
 {
     alarm[0] = 0;
+    printf("Alarmen er blevet slukket\n");
+
 }
 
+void power_off(void)
+{
+    menuRunner = 0;
+    printf("slukker program\n");
+
+}
