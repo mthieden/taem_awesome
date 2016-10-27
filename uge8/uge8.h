842bfc1515d41da932ef27d4c06823a0481a17d4
mthieden <m.thieden@gmail.com>
2016-10-27 19:28:27 +0200

    mere lol

--- uge8/uge8.h
+++ uge8/uge8.h
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 
 typedef struct contestant{
-    char name[30];
+    char name[64];
     int accScore; // Accureacy score
     int ausScore; // Aussie-Round score
     int endScore; // Endurance score
@@ -23,17 +23,25 @@ typedef struct llist {
 	node_t	*tail;
 } llist_t;
 
-/* llist_init initializes a new llist */
+/* llist_init initializes a new linked list */
 void llist_init(llist_t *ll);
 
-/* llist_put adds the item to the tail of the llist */
+/* llist_put adds the item to the tail of the inked linkedlist */
 void llist_put(llist_t *ll, const_t item);
 
-/* llist_pop pops and returns the item of the node from the llist*/
+/* llist_pop pops and returns the item of the node from the linkedlist*/
 const_t  llist_pop(llist_t *ll, node_t *node_ptr);
 
 /* llist_search returns the node with the requested item */
 node_t *llist_search(llist_t *ll, const_t item);
 
-/* llist_print prints the llist */
+/* llist_pop_item takes a item as input and pops the element with the item*/
+const_t llist_pop_item(llist_t *ll, const_t item );
+    
+
+/* llist_print prints the linkedlist */
 void llist_print(llist_t *ll);
+
+/*   llist_summary prints max, min and average of values*/
+void llist_summary(llist_t *ll);
+
--- uge8/uge81.c
+++ uge8/uge81.c
@@ -9,9 +9,10 @@ void llist_init(llist_t *ll)
 		exit(EXIT_FAILURE);
 	}
 
+    // the next node of the linked list is set to null
 	new->next = NULL;
 	ll->head = ll->tail = new;
-}
+}// end of llist_init
 
 void llist_put(llist_t *ll, const_t item) 
 {
@@ -24,10 +25,10 @@ void llist_put(llist_t *ll, const_t item)
     new->item = item;
 	new->next = NULL;
 
-	/* add the new node to the tail */
+	/* add the new node to the tail of the linked list*/
 	ll->tail->next = new;
 	ll->tail = new;
-}
+}// end of llist_put
 
 const_t llist_pop(llist_t *ll, node_t *node) 
 {
@@ -38,6 +39,7 @@ const_t llist_pop(llist_t *ll, node_t *node)
 
 	if (old->next == NULL) 
     {
+        //prints an error message and terminates
         puts("pop failed, empty list");
         exit(EXIT_FAILURE);
 	}
@@ -52,7 +54,7 @@ const_t llist_pop(llist_t *ll, node_t *node)
             old->next = new->next;
             const_t item = new->item;
             free(new);
-            /* update the head and free the old memory */
+            // update the linked list and free the old memory 
             return item;
         }
         
@@ -64,48 +66,47 @@ const_t llist_pop(llist_t *ll, node_t *node)
     
     puts("pop failed, didn't find the node");
     exit(EXIT_FAILURE);
-}
+}// end of llist_pop
 
 node_t *llist_search(llist_t *ll, const_t item)
 {
 
     node_t *node_search = ll->head;
+    //chescks if the list is empty
     if (node_search->next == NULL) 
     {
-		return NULL; // llist was empty 
+		return NULL;  
 	}
     
-    
+    // the element used to iterate
     node_search = node_search->next;
     
-    if(memcmp((void*)&node_search->item, (void*)&item, sizeof(const_t))==0)
-    {
-        return  node_search;
-    }        
-
-
     while (node_search != NULL)
     {
+        // memcmp takes two voidpointers and a length, and compare the data
+        // from the pointer to the length
         if(memcmp((void*)&node_search->item, (void*)&item, sizeof(const_t))==0)
         {
             return node_search;
         }
         
+        // node_search is set to the next element in the linked list
         node_search =node_search->next;
     }
     
     return NULL;   // did not find the item
 
-}
+}//end of llist_search
 
 const_t llist_pop_item(llist_t *ll, const_t item )
 {
     return llist_pop(ll, llist_search(ll, item) );
 
-}
+}//end of llist_pop_item
 
 void llist_print(llist_t *ll)
 {
+    // node_search is set to the first element of the linked list
     node_t *node_search = ll->head;
     
     if (node_search->next == NULL) 
@@ -127,7 +128,7 @@ void llist_print(llist_t *ll)
     }
     printf("\n");
 
-}
+}//end of llist_print
 
 
 void read_data(char *file, llist_t *ll)
@@ -151,12 +152,12 @@ void read_data(char *file, llist_t *ll)
          }
  
  
-         //Tokenize r, at "," and assign to 
+         //Tokenize file_input, at "," and insert the elements into the linked list  
          char *tokenPtr = strtok(file_input, ",");
          strcpy(contestant.name, tokenPtr);
  
          tokenPtr = strtok(NULL, ",");
-         contestant.accScore = atoi(tokenPtr);
+         contestant.accScore = atoi(tokenPtr); // never use atoi if you like your clients :(
  
          tokenPtr = strtok(NULL, ",");
          contestant.ausScore = atoi(tokenPtr);
@@ -170,21 +171,95 @@ void read_data(char *file, llist_t *ll)
    }
 
    fclose(fp);
-}
+}// end of read_data
 
+void llist_summary(llist_t *ll)
+{
+    node_t *node_search = ll->head;
+    
+    int acc_min, acc_max, acc_ave = 0;
+    int aus_min, aus_max, aus_ave = 0;
+    int end_min, end_max, end_ave = 0;
+    int i = 0;
+
+    if (node_search->next == NULL) 
+    {
+        puts("print fail, tried to print a empty list");
+        exit(EXIT_FAILURE);
+	}
+    node_search = node_search->next;
+    acc_min = acc_max = node_search->item.accScore; 
+    aus_min = aus_max = node_search->item.ausScore;
+    end_min = end_max = node_search->item.endScore;
+    
+
+    while (node_search != NULL)
+    {  
+        //checks acc value and find max and minvalues 
+        if(acc_min > node_search->item.accScore)
+        {
+            acc_min = node_search->item.accScore;
+        }
+        if(acc_max < node_search->item.accScore)
+        {
+            acc_max = node_search->item.accScore;
+        }
+        // add element to calculate average
+        acc_ave += node_search->item.accScore;
+
+
+        //checks aus value and find max and minvalues 
+        if(aus_min > node_search->item.ausScore)
+        {
+            aus_min = node_search->item.ausScore;
+        }
+        if(aus_max < node_search->item.ausScore)
+        {
+            aus_max = node_search->item.ausScore;
+        }
+        aus_ave += node_search->item.ausScore;
+
+
+        //checks end value and find max and minvalues 
+        if(end_min > node_search->item.endScore)
+        {
+            end_min = node_search->item.endScore;
+        }
+        if(end_max < node_search->item.endScore)
+        {
+            end_max = node_search->item.endScore;
+        }
+        end_ave += node_search->item.endScore;
+
+
+        i++;
+        node_search = node_search->next;
+    }
+    //calculate the average
+    float acc_average = (float)acc_ave/(float)i; 
+    float aus_average = (float)aus_ave/(float)i;
+    float end_average = (float)end_ave/(float)i;
+
+    //prints the found values
+    printf("%s %d %s %d %s %.2f %s %d \n","accScore min: ", acc_min,"accScore max: ", acc_max,"accScore average: ", acc_average,"total", acc_ave);
+    printf("%s %d %s %d %s %.2f %s %d \n","ausScore min: ", aus_min,"ausScore max: ", aus_max,"ausScore average: ", aus_average,"total", aus_ave);
+    printf("%s %d %s %d %s %.2f %s %d \n","endScore min: ", end_min,"endScore max: ", end_max,"endScore average: ", end_average,"total", end_ave);
+}// end of llist_summary
 
 
 int main(int argc, char **argv) 
 {
-
 
     char *file = "data.csv";
 	llist_t ll;
 
 	llist_init(&ll);
     read_data(file,&ll);    
-
+    puts("printing linked list");
     llist_print(&ll); 
+    
+    puts("printing linked list");
+    llist_summary(&ll);
     
     /* testing the different funkctions
 	const_t val1 = {.name = "Ben and jerry", .accScore = 42, .ausScore = 432, .endScore = 1011};
