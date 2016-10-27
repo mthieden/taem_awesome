1c3d0eb356b9bd140d676d483d7edc39d547134e
Jonas Holm <jonas.dk.1990@gmail.com>
2016-10-27 10:21:00 +0200

    Templates uge 8

---
+++ uge8/uge82.c
@@ -0,0 +1,7 @@
+/*
+    Opgave 8.2
+    Gruppe "taem awesome" (ja det er stavet forkert med vilje)
+        s153460 Jonas Ladefoged Holm
+        s113070 David Bjerre Bjørklund
+        s164920 Markus Visvaldis Ingemann Thieden
+*/
