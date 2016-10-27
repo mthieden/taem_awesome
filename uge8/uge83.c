3bd6eb5f0f713df6a480baaf50d8db911a252932
DavidBjerreBjoerklund <dkcookie@gmail.com>
2016-10-27 18:01:57 +0200

    Uge83 added

--- uge8/uge83.c
+++ uge8/uge83.c
@@ -5,3 +5,76 @@
         s113070 David Bjerre Bjørklund
         s164920 Markus Visvaldis Ingemann Thieden
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+int main()
+{
+    struct contestant{
+        char name[64];
+        int accScore; // Accuracy score
+        int ausScore; // Aussie-Round score
+        int endScore; // Endurance score
+    } Contestant;
+    
+    char fileInput[64];
+    
+    // Declare File Position Pointer
+    FILE *fOutAcPtr;
+    FILE *fOutAuPtr;
+    FILE *fOutEnPtr;
+    FILE *fInPtr;
+    
+    if((fInPtr = fopen ("data.csv", "r")) == NULL)
+    {
+        printf("Cannot read file!");
+    }
+    else
+    {
+        // Open files for write seperate values
+        fOutAcPtr = fopen("Accuracy.csv", "w");
+        fOutAuPtr = fopen("Aussie.csv", "w");
+        fOutEnPtr = fopen("Endurance.csv", "w");
+        
+        // Parse every line and save in struct array
+        while(!feof(fInPtr)){
+            // Reads to end of line, max 60 characters
+            fgets(fileInput, 64, fInPtr);
+            
+            // Remove '\n' from line
+            if(iscntrl(fileInput[strlen(fileInput) - 1])){
+                fileInput[strlen(fileInput) - 1] = '\0';
+            }
+            
+            //Tokenize r, at "," and assign to
+            char *tokenPtr = strtok(fileInput, ",");
+            strcpy(Contestant.name, tokenPtr);
+            
+            tokenPtr = strtok(NULL, ",");
+            Contestant.accScore = atoi(tokenPtr);
+            
+            tokenPtr = strtok(NULL, ",");
+            Contestant.ausScore = atoi(tokenPtr);
+            
+            tokenPtr = strtok(NULL, ",");
+            Contestant.endScore = atoi(tokenPtr);
+            
+            fprintf(fOutAcPtr, "%s, %d\n", Contestant.name, Contestant.accScore);
+            fprintf(fOutAuPtr, "%s, %d\n", Contestant.name, Contestant.accScore);
+            fprintf(fOutEnPtr, "%s, %d\n", Contestant.name, Contestant.endScore);
+        }
+        
+        fclose(fOutAcPtr);
+        fclose(fOutAuPtr);
+        fclose(fOutEnPtr);
+    }
+    
+    
+    fclose(fInPtr);
+    
+    return(0);
+}
