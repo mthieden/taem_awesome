c0fc6104c63d5b452360d8a023efa4b461e5d595
mthieden <m.thieden@gmail.com>
2016-10-27 15:44:46 +0200

    linked list.h

--- uge8/uge8.h
+++ uge8/uge8.h
@@ -1,12 +1,19 @@
 #include <assert.h>
 #include <error.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
+typedef struct contestant{
+    char name[30];
+    int accScore; // Accureacy score
+    int ausScore; // Aussie-Round score
+    int endScore; // Endurance score
+}const_t;
 
 
 typedef struct node {
-	void *item;
+    const_t item;
 	struct node *next;
 } node_t;
 
@@ -19,13 +26,13 @@ typedef struct llist {
 void llist_init(llist_t *ll);
 
 /* llist_put adds the item to the tail of the llist */
-void llist_put(llist_t *ll, void *item);
+void llist_put(llist_t *ll, const_t item);
 
 /* llist_pop pops and returns the item of the node from the llist*/
-void *llist_pop(llist_t *ll, node_t *node_ptr);
+const_t  llist_pop(llist_t *ll, node_t *node_ptr);
 
 /* llist_search returns the node with the requested item */
-node_t *llist_search(llist_t *ll, void *item);
+node_t *llist_search(llist_t *ll, const_t item);
 
 /* llist_print prints the llist */
 void llist_print(llist_t *ll);
